201712f49e32574bd7ed3f491178763e022bd071
vinaybandakka <vinay.bandakka@symphonyteleca.com>
2017-10-06 13:35:59 +0530

    inital java project files

---
+++ src/Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+	public int lenght;
+	public int width;
+
+	public Rectangle(int lenght, int width) {
+	  this.lenght = length;
+	  this.width = width;
+	}
+
+	public int getArea() {
+	  return length * width;
+	}
+
+	public int getPerimeter() {
+	  return 2 * (length + width);
+	}
+}
---
+++ src/Rectangulator.java
@@ -0,0 +1,11 @@
+public class Rectangulator {
+  public static void main(String[] args) {
+    int lenght = Integer.parseInt(arg[0]);
+    int wight = Integet.parseInt(args[1]);
+
+    Rectagle myRectagle = new Rectangle(length, width);
+
+    String output = String.format(" ################## Your Rectangle #########\n\nLenth: %d\nWidth: %d \nArea: %d\nPerimeter: %d\n\n", myRectable.lenth, myRectangle.width, myRectangle.getArea(), mtRectable.getPerimeter()
+    System.out.println(output); 
+  }
+}
